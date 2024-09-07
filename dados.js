a60b4a7d9a5aad1e09918099c8d628e2fd99197d
StoneNobre <71931206+StoneNobre@users.noreply.github.com>
2024-09-07 16:58:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, conhecida carinhosamente como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, ela já coleciona títulos importantes, incluindo uma medalha de prata nos Jogos Olímpicos de Tóquio 2020, tornando-se a mais jovem medalhista olímpica brasileira da história. Com sua habilidade e determinação, Rayssa inspira jovens atletas e quebra barreiras no esporte, mostrando que o skate é para todos.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional",
+    link: "https://pt.wikipedia.org/wiki.org/wiki/Rebeca_Andrade", // Possível erro de digitação: link duplicado
+    tags: "ginástica ginastica ouro"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional e se consolidando como uma das principais atletas do judô nacional.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoca judoka"
+  }
+];
